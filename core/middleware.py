e02f7e0521de8af43d233d1b48b36ff98f218cb5
Ishimistic <ishitav653@gmail.com>
2026-05-02 13:13:24 +0530

    Rate Limiter with limits on headers is been implemented.

--- core/middleware.py
+++ core/middleware.py
@@ -4,7 +4,7 @@ from core.redis_client import redis_client
 class RateLimitMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.LIMIT = 5
+        self.LIMIT = 10
         self.WINDOW = 30
         
     def __call__(self, request):
@@ -16,6 +16,7 @@ class RateLimitMiddleware:
         
         count = redis_client.get(redis_key) # get current count
         
+        
         if count is None:
             redis_client.set(redis_key, 1, ex=self.WINDOW)
             count = 1
@@ -23,16 +24,26 @@ class RateLimitMiddleware:
             count = redis_client.incr(redis_key)
             
         
+        remainig = max(0, self.LIMIT - int(count))
+            
+        
         print(f"IP: {client_ip}, Count: {count}")
             
         
         if int(count) > self.LIMIT:
             return JsonResponse(
-                {"error": "Too many requests."}
-                , status=429
+                {"error": "Too many requests."}, 
+                status=429,
+                headers={
+                  "X-RateLimit-Limit": str(self.LIMIT),
+                  "X-RateLimit-Remaining": "0",
+                }
             )
             
         response = self.get_response(request)
+        response["X-RateLimit-Limit"] = str(self.LIMIT)
+        response["X-RateLimit-Remaining"] = str(remainig)
+        
         return response
     
     def get_client_ip(self, request):
